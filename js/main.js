f44a2c39ff91324aa8019056b920563b7774a5c6
ahmed <ahmedSamyAlashmony1@gmail.com>
2024-11-30 20:30:15 +0200

    making site never repeated

--- js/main.js
+++ js/main.js
@@ -10,6 +10,15 @@ if(localStorage.getItem('sites')){
 
 function addSite(){
     if(Validation(siteName) && Validation(siteUrl)){
+
+        for (var i = 0; i < siteList.length; i++) {
+            if (siteList[i].sName.toLowerCase() === siteName.value.toLowerCase()) {
+                siteName.nextElementSibling.innerHTML = "The site name is already taken. Add anoter site.";
+                siteName.nextElementSibling.classList.remove("d-none"); 
+                return; 
+            }
+        }
+
     sitObj = {
         id:Date.now(),
         sName:siteName.value,
@@ -19,13 +28,18 @@ function addSite(){
     localStorage.setItem("sites",JSON.stringify(siteList));
     display();
     clearForm();
-}
+    
+    siteName.nextElementSibling.classList.add('d-none');
+
+    }
+    siteName.classList.remove('is-valid');
+    siteUrl.classList.remove('is-valid');
 }
 
 function display(){
     box='';
     for(var i=0 ; i<siteList.length ; i++ ){
-        box+= `<tr class="border-top d-table-row pt-5 mt-5">
+        box+= `<tr class="border-top  pt-5 mt-5">
                 <td class="py-3">${i+1}</td>
                 <td class="text-capitalize">${siteList[i].sName}</td>
                 <td><a href="${siteList[i].sUrl}" target="blank"><button class="btn btn-primary white"><i class="fa-regular fa-eye pe-2"></i>Visite</button></a></td>
@@ -46,7 +60,6 @@ function deleteIteam(id){
     localStorage.setItem('sites',JSON.stringify(siteList));
     display();
 }
-
 
 function Validation(input){
     var Regex = {
